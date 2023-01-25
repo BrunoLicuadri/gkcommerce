9acad4de0f7c16c0f1c12390d5ee189447adb211
BrunoLicuadri <blicuadri@gmail.com>
2023-01-25 15:02:23 -0300

    Payment entity and its relation one-to-one to Order entity created

--- src/main/java/com/godknows/gkcommerce/entities/Order.java
+++ src/main/java/com/godknows/gkcommerce/entities/Order.java
@@ -20,6 +20,9 @@ public class Order {
     @JoinColumn(name="client_id")
     private User client;
 
+    @OneToOne(mappedBy="order",cascade=CascadeType.ALL)
+    private Payment payment;
+
     public Order(){
     }
 
