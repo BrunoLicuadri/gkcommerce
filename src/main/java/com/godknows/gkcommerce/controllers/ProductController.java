65eb351bf7987bc6e94be5832d165e3dadc678b5
BrunoLicuadri <blicuadri@gmail.com>
2023-01-26 17:19:12 -0300

    customizing Controller layer response with ResponseEntity

--- src/main/java/com/godknows/gkcommerce/controllers/ProductController.java
+++ src/main/java/com/godknows/gkcommerce/controllers/ProductController.java
@@ -5,8 +5,11 @@ import com.godknows.gkcommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -18,20 +21,22 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping(value="/{id}")
-    public ProductDTO findById(@PathVariable Long id){
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
         ProductDTO dto = service.findById(id);
-        return dto;
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable){
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+        Page <ProductDTO> dto= service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 
     @PostMapping
-    public ProductDTO insert (@RequestBody ProductDTO dto){
+    public ResponseEntity<ProductDTO> insert (@RequestBody ProductDTO dto){
         dto = service.insert(dto);
-        return dto;
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 
