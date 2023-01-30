bcb476624f01cb2d7b6eeb83f5737a3cc73d8f9e
BrunoLicuadri <blicuadri@gmail.com>
2023-01-30 11:07:24 -0300

    Customized exception response for bad Controller Request

---
+++ src/main/java/com/godknows/gkcommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.godknows.gkcommerce.controllers.handlers;
+
+import com.godknows.gkcommerce.dto.CustomError;
+import com.godknows.gkcommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound (ResourceNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
