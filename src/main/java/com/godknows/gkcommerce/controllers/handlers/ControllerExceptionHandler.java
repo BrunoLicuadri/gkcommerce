6ce319712b14abe96e66042d236f4496c250073a
BrunoLicuadri <blicuadri@gmail.com>
2023-01-30 12:14:05 -0300

    Created remained customized excpetions handler

--- src/main/java/com/godknows/gkcommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/godknows/gkcommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.godknows.gkcommerce.controllers.handlers;
 
 import com.godknows.gkcommerce.dto.CustomError;
+import com.godknows.gkcommerce.services.exceptions.DatabaseException;
 import com.godknows.gkcommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,6 +18,13 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound (ResourceNotFoundException e, HttpServletRequest request){
         HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database (DatabaseException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
--- src/main/java/com/godknows/gkcommerce/services/ProductService.java
+++ src/main/java/com/godknows/gkcommerce/services/ProductService.java
@@ -4,11 +4,16 @@ import com.godknows.gkcommerce.dto.ProductDTO;
 import com.godknows.gkcommerce.entities.Product;
 import com.godknows.gkcommerce.repositories.ProductRepository;
 
+import com.godknows.gkcommerce.services.exceptions.DatabaseException;
 import com.godknows.gkcommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -43,16 +48,29 @@ public class ProductService {
 
     @Transactional
     public ProductDTO update (Long id, ProductDTO dto){
-        Product entity = repository.getReferenceById(id);
-        copyDtoToEntity(dto, entity);
-        entity = repository.save(entity);
-        return new ProductDTO(entity);
+        try{
+            Product entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new ProductDTO(entity);
+        }
+        catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException("Recurso Não Encontrado");
+            }
     }
 
-
-    @Transactional
+    @Transactional (propagation = Propagation.SUPPORTS)
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException("Recurso Não Encontrado");
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de Integridade Referencial");
+        }
+
     }
 
 
