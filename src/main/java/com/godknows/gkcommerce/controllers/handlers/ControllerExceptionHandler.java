bfddf726fa1dee376bcedf6211e021a0314e19f2
BrunoLicuadri <blicuadri@gmail.com>
2023-01-30 15:22:08 -0300

    Completed bean validation exceptions response

--- src/main/java/com/godknows/gkcommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/godknows/gkcommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import com.godknows.gkcommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -34,9 +35,10 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<CustomError> methodArgumentNotValid (MethodArgumentNotValidException e, HttpServletRequest request){
         HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
-        ValidationError err = new ValidationError (Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
-        err.addError("name", "Msg de teste");
-        err.addError("price", "Preço Inválido");
+        ValidationError err = new ValidationError (Instant.now(), status.value(), "Dados Inválidos", request.getRequestURI());
+        for (FieldError f : e.getBindingResult().getFieldErrors()){
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 
