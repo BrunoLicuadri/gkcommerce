c41959f85da715524bed3bbd1de9f0306747daec
BrunoLicuadri <blicuadri@gmail.com>
2023-02-22 02:37:54 -0300

    SearchByName method added

--- src/main/java/com/godknows/gkcommerce/repositories/ProductRepository.java
+++ src/main/java/com/godknows/gkcommerce/repositories/ProductRepository.java
@@ -1,8 +1,16 @@
 package com.godknows.gkcommerce.repositories;
 
 import com.godknows.gkcommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT obj " +
+            "FROM Product obj " +
+            "WHERE UPPER (obj.name) LIKE UPPER(CONCAT('%', :name,'%'))")
+    public Page<Product> searchByName (String name, Pageable pageable);
 
 }
