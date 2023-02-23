d3e7778917966fd4bec5b62c2d424280f666d427
BrunoLicuadri <blicuadri@gmail.com>
2023-02-23 11:26:25 -0300

    Role entiy created, its associateion to User entity, seeds

--- src/main/java/com/godknows/gkcommerce/entities/User.java
+++ src/main/java/com/godknows/gkcommerce/entities/User.java
@@ -4,7 +4,9 @@ import javax.persistence.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name="tb_user")
@@ -26,6 +28,12 @@ public class User {
 
     @OneToMany(mappedBy="client")
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+                joinColumns = @JoinColumn(name="user_id"),
+                inverseJoinColumns = @JoinColumn(name="role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
         this.id = id;
@@ -86,5 +94,9 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
     }
 }
