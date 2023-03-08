34f1babfa22ed3d54cce634b80579c394a4b8332
BrunoLicuadri <blicuadri@gmail.com>
2023-03-08 19:52:25 -0300

    Implemented Spring Security Model

--- src/main/java/com/godknows/gkcommerce/entities/User.java
+++ src/main/java/com/godknows/gkcommerce/entities/User.java
@@ -1,16 +1,16 @@
 package com.godknows.gkcommerce.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name="tb_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -84,8 +84,38 @@ public class User {
         this.birthDate = birthDate;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
---
+++ src/main/java/com/godknows/gkcommerce/services/UserService.java
@@ -0,0 +1,22 @@
+package com.godknows.gkcommerce.services;
+
+import com.godknows.gkcommerce.entities.User;
+import com.godknows.gkcommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByEmail(username);
+        if(user == null){
+           throw new UsernameNotFoundException("Email not found");
+        }
+        return user;
+    }
+}
