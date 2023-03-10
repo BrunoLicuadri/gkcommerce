d2155fa1b743adfe0c64a34a240072419b1aa3e0
BrunoLicuadri <blicuadri@gmail.com>
2023-03-10 12:23:22 -0300

    Added a missed @Embeddable in OrderItemPK class

--- src/main/java/com/godknows/gkcommerce/entities/OrderItemPK.java
+++ src/main/java/com/godknows/gkcommerce/entities/OrderItemPK.java
@@ -1,9 +1,11 @@
 package com.godknows.gkcommerce.entities;
 
+import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.io.Serializable;
 
+@Embeddable
 public class OrderItemPK implements Serializable {
     private static final long serialVersionUID=1L;
 
